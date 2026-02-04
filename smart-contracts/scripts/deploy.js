97dabbc3c94d9c67beda84518a8ac7429ba94b67
Divya Upadhyay <154431837+DIVYA026-HUB@users.noreply.github.com>
2026-02-04 19:50:09 +0530

    Add files via upload

---
+++ smart-contracts/scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+    const Voting = await hre.ethers.getContractFactory("Voting");
+    const voting = await Voting.deploy();
+
+    await voting.waitForDeployment();
+
+    console.log("Voting Contract deployed to:", await voting.getAddress());
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
